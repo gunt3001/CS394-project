16297542677c2f15baea9b0869b4068b8f48dd08
Napat R <nr879@nyu.edu>
2014-03-19 20:50:03 -0400

    Added Class for Songs Table View Controller

---
+++ Meso/Meso/MSPSongsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSPSongsTableViewController.h
+//  Meso
+//
+//  Created by Gun on 19/3/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSPSongsTableViewController : UITableViewController
+
+@end
