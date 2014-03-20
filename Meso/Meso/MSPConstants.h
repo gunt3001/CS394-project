2b42d4da6bb9c0c15269b9894c90b8e5e45a06ed
Napat R <nr879@nyu.edu>
2014-03-20 01:34:01 -0400

    Added file to store constants

---
+++ Meso/Meso/MSPConstants.h
@@ -0,0 +1,13 @@
+//
+//  MSPConstants.h
+//  Meso
+//
+//  Created by Gun on 20/3/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSPConstants : NSObject
+
+@end
---
+++ Meso/Meso/MSPConstants.m
@@ -0,0 +1,13 @@
+//
+//  MSPConstants.m
+//  Meso
+//
+//  Created by Gun on 20/3/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+
+#import "MSPConstants.h"
+
+@implementation MSPConstants
+
+@end
