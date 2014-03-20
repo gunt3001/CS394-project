a2a064dea5ec5b64a22cc23cf41eacea6c89f279
Napat R <nr879@nyu.edu>
2014-03-20 03:17:17 -0400

    Added placeholder back button + subclass for now playing view

---
+++ Meso/Meso/MSPNowPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSPNowPlayingViewController.h
+//  Meso
+//
+//  Created by Gun on 20/3/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSPNowPlayingViewController : UIViewController
+
+@end
---
+++ Meso/Meso/MSPNowPlayingViewController.m
@@ -0,0 +1,53 @@
+//
+//  MSPNowPlayingViewController.m
+//  Meso
+//
+//  Created by Gun on 20/3/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+
+#import "MSPNowPlayingViewController.h"
+
+@interface MSPNowPlayingViewController ()
+
+@end
+
+@implementation MSPNowPlayingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonBack:(id)sender {
+    // Back button action - dismiss current view
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
