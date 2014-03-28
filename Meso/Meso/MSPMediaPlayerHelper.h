eca3d4835c9933fde65606cafa0032cb0ec89c63
Napat R <nr879@nyu.edu>
2014-03-28 15:51:32 -0400

    added empty helper class

---
+++ Meso/Meso/MSPMediaPlayerHelper.h
@@ -0,0 +1,16 @@
+//
+//  MSPMediaQueryHelper.h
+//  Meso
+//
+//  Created by Gun on 3/28/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+//
+//  A class to help with querying and playing media items
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSPMediaPlayerHelper : NSObject
+
+@end
---
+++ Meso/Meso/MSPMediaPlayerHelper.m
@@ -0,0 +1,13 @@
+//
+//  MSPMediaQueryHelper.m
+//  Meso
+//
+//  Created by Gun on 3/28/14.
+//  Copyright (c) 2014 Napat R. All rights reserved.
+//
+
+#import "MSPMediaPlayerHelper.h"
+
+@implementation MSPMediaPlayerHelper
+
+@end
