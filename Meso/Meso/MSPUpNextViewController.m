d104002cf7ec86ac63d73a9bbdb21b38fa071065
Napat Ratanakul <nr879@nyu.edu>
2014-04-01 15:27:13 -0400

    Add remove-reordering controls

--- Meso/Meso/MSPUpNextViewController.m
+++ Meso/Meso/MSPUpNextViewController.m
@@ -35,7 +35,8 @@
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -64,7 +65,7 @@
     
     // As long as we still have upcoming songs, we show them
     // with a limit of: UPNEXT_COUNT
-    while (next != nil && upcomingCount <= UPNEXT_COUNT){
+    while (next && upcomingCount < UPNEXT_COUNT){
         upcomingCount++;
         nextItemIndex++;
         next = [iPodMusicPlayer nowPlayingItemAtIndex:nextItemIndex];
@@ -89,43 +90,56 @@
 }
 
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
+        
+        // Modify Table
+        [tableView beginUpdates];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        
+        // If we still have an upcoming song, add it to table
+        MPMusicPlayerController* iPodMusicPlayer = [((MSPAppDelegate*)[[UIApplication sharedApplication] delegate]) sharedPlayer];
+        NSInteger nextItemIndex = [iPodMusicPlayer indexOfNowPlayingItem] + 1 + indexPath.row;
+        MPMediaItem* next = [iPodMusicPlayer nowPlayingItemAtIndex:nextItemIndex];
+        if (next){
+            NSIndexPath* lastIndex = [NSIndexPath indexPathForRow:([tableView numberOfRowsInSection:0] - 1) inSection:0];
+            [tableView insertRowsAtIndexPaths:@[lastIndex] withRowAnimation:UITableViewRowAnimationFade];
+        }
+        [tableView endUpdates];
+        
+        
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
-/*
+
+
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
 {
 }
-*/
 
-/*
+
+
 // Override to support conditional rearranging of the table view.
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
-*/
+
 
 /*
 #pragma mark - Navigation
