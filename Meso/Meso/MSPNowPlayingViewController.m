c47e4eb20cec7c144fed17b9fcdfca3eec36733f
Napat R <nr879@nyu.edu>
2014-04-09 17:35:12 -0400

    Remove unused autogenerated methods

--- Meso/Meso/MSPNowPlayingViewController.m
+++ Meso/Meso/MSPNowPlayingViewController.m
@@ -36,22 +36,6 @@
     UIViewController*            menuViewController;
 }
 
-#pragma mark - Xcode Generated
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 #pragma mark - Initialization
 
 - (void)viewDidLoad
